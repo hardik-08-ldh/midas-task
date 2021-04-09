d8b294ebe42e94c87723190e50a643cc861fc366
Hardik <punjhardik2000@gmail.com>
2021-04-09 22:54:32 +0530

    added comments

--- midas/settings.py
+++ midas/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 # decouple used so secret _key protected
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
